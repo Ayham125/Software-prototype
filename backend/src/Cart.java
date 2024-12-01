129b18a5af8852bbc0802443fb094b2f7772f286
RakanBarakat <realrakanhani2@gmail.com>
2024-12-01 14:38:54 +0400

    editted backend file

---
+++ backend/src/Cart.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private String[][] items = new String[2][4];
+
+    public void addItem(String itemId, String SID) {
+        //look for SID then concat itemID with " "
+        for (String items : items[0][4]){
+
+        }
+    }
+    public void removeItem(String itemId) {
+        items.remove(itemId);
+    }
+    public void viewItems(List<String> items){
+
+    }
+
+}
