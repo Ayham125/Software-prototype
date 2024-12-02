b60adea861f8c5d58f04cda34b6f4bfd94271e4f
RakanBarakat <realrakanhani2@gmail.com>
2024-12-02 13:22:05 +0400

    finished removeItems function

--- backend/src/Cart.java
+++ backend/src/Cart.java
@@ -28,14 +28,26 @@ public class Cart {
         mainTable[1][3] = "0";
         mainTable[2][3] = "0";
         mainTable[3][3] = "0";
+        //initalize products and prices
+        products[0][0] = "Apple";      // Item ID
+        products[0][1] = "5";        // Price
+        products[1][0] = "Bread";
+        products[1][1] = "2";
+        products[2][0] = "Milk";
+        products[2][1] = "3";
+        products[3][0] = "Eggs";
+        products[3][1] = "10";
+        products[4][0] = "Cheese";
+        products[4][1] = "5";
     }
-    public void addItem(String itemId, String SID, int price) {
+
+    public void addItem(String itemId, String SID) {
         //look for SID then concat itemID with " "
-        for (int i = 0; i<4; i++){
+        for (int i = 0; i < 4; i++) {
             int productPrice = 0;
-            if (mainTable[i][1].equals(SID)){
-                for(int y = 0; y<10;y++){
-                    if (products[y][0].equals(itemId)){
+            if (mainTable[i][1].equals(SID)) {
+                for (int y = 0; y < 10; y++) {
+                    if (products[y][0].equals(itemId)) {
                         productPrice = Integer.parseInt(products[y][1]);
                         break;
                     }
@@ -52,27 +64,30 @@ public class Cart {
     }
 
     public void removeItem(String itemId, String SID) {
-        for (int i = 0; i < 4; i++){
-            if (mainTable[i][1].equals(SID)){
+        for (int i = 0; i < 4; i++) {
+            if (mainTable[i][1].equals(SID)) {
                 String[] items = mainTable[i][2].split(" ");
                 StringBuilder newItems = new StringBuilder();
-                for (String item : items){
-                    if(!item.equals(itemId)){
-                        if (newItems.length()>0){
+                for (String item : items) {
+                    if (!item.equals(itemId)) {
+                        if (newItems.length() > 0) {
                             newItems.append(" ");
                         }
                         newItems.append(item);
                     }
                 }
+
                 mainTable[i][2] = newItems.toString().trim();
-                break;
+                for (int y = 0; y < 10; y++) {
+                    if (products[y][0].equals(itemId)) {
+                        int productPrice = Integer.parseInt(products[y][1]);
+                        int currentTotal = Integer.parseInt(mainTable[i][3]);
+                        mainTable[i][3] = Integer.toString(currentTotal - productPrice);
+                        break;
+                    }
+                }
+                return;
             }
         }
     }
-
-
-    public void viewItems(){
-
-    }
-
 }
