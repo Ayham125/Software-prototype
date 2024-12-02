a867f76cb60f7ba5e42e9df9adc82a0b8707b902
RakanBarakat <realrakanhani2@gmail.com>
2024-12-02 14:18:36 +0400

    refactored

--- Main/src/Main1.java
+++ Main/src/Main1.java
@@ -1,14 +1,21 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main1 extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Styling.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("JavaFX With SceneBuilder");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
     }
 }
