b5adffe1ff7499a160c1ff1a3072b804758de214
alimohamed05 <alibadie338@gmail.com>
2024-12-02 13:04:01 +0400

    adjusted addItem to update the total after adding an item

--- backend/src/Cart.java
+++ backend/src/Cart.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Cart {
 
     public String[][] mainTable = new String[4][4];
+    public String[][] products = new String[10][2];
 
     public Cart() {
         //fixed student names in the first column
@@ -13,7 +14,7 @@ public class Cart {
         mainTable[2][0] = "Student 3";
         mainTable[3][0] = "Student 4";
         //fixed student IDs in the second column
-        mainTable[0][1] = "100000000";
+        mainTable[0][1] = "1";
         mainTable[1][1] = "2";
         mainTable[2][1] = "3";
         mainTable[3][1] = "4";
@@ -22,12 +23,29 @@ public class Cart {
         mainTable[1][2] = "";
         mainTable[2][2] = "";
         mainTable[3][2] = "";
+        //initialize total values to 0
+        mainTable[0][3] = "0";
+        mainTable[1][3] = "0";
+        mainTable[2][3] = "0";
+        mainTable[3][3] = "0";
     }
-    public void addItem(String itemId, String SID) {
+    public void addItem(String itemId, String SID, int price) {
         //look for SID then concat itemID with " "
         for (int i = 0; i<4; i++){
+            int productPrice = 0;
             if (mainTable[i][1].equals(SID)){
+                for(int y = 0; y<10;y++){
+                    if (products[y][0].equals(itemId)){
+                        productPrice = Integer.parseInt(products[y][1]);
+                        break;
+                    }
+                }
+                //mainTable[i][3] is converted from string to integer
+                //we then add the converted result to the product price and store it in the variable total
+                int total = productPrice + Integer.parseInt(mainTable[i][3]);
+                String tableTotal = Integer.toString(total);
                 mainTable[i][2] = mainTable[i][2] + " " + itemId;
+                mainTable[i][3] = tableTotal;
                 return;
             }
         }
@@ -51,6 +69,8 @@ public class Cart {
             }
         }
     }
+
+
     public void viewItems(){
 
     }
