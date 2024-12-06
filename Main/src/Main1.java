0899a7ce6b54737021610ab640dc9db1ea167fa3
mlk26 <mlkdjaoui@gmail.com>
2024-12-06 17:56:16 +0400

    Make Homepage.fxml

--- Main/src/Main1.java
+++ Main/src/Main1.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 public class Main1 extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Styling.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Homepage.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setTitle("JavaFX With SceneBuilder");
